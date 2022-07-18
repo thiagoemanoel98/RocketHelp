76dd0e0337299eeea47eb2cc27d47404e6e3775e
Thiago Emanoel <thiagoemanoel448@gmail.com>
2022-07-18 00:51:13 -0300

    Feat: Tela de login

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import { NativeBaseProvider, StatusBar } from "native-base";
+import React from "react";
+import {THEME} from './src/styles/theme';
+import { SignIn } from "./src/screens/SignIn";
+import { Loading } from "./src/components/Loading";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
+
+  return (
+    <NativeBaseProvider theme = {THEME}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded? <SignIn /> : <Loading/>}
+    </NativeBaseProvider>
+  );
+}
